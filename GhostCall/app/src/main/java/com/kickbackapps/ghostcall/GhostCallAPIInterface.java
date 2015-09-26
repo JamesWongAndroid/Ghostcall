745b909572fa59cd8bbe624878c8ea138a36c34d
JamesWongAndroid <james.wong.android@gmail.com>
2015-09-25 20:39:38 -0400

    bunch of fixes, change the package name

---
+++ GhostCall/app/src/main/java/com/kickbackapps/ghostcall/GhostCallAPIInterface.java
@@ -0,0 +1,87 @@
+package com.kickbackapps.ghostcall;
+
+import com.kickbackapps.ghostcall.BackgroundEffects.BackgroundEffectsData;
+import com.kickbackapps.ghostcall.Numbers.ExtendObject;
+import com.kickbackapps.ghostcall.Numbers.Message;
+import com.kickbackapps.ghostcall.SoundEffects.SoundEffectsData;
+import com.kickbackapps.ghostcall.User.CallStatus;
+import com.kickbackapps.ghostcall.User.UserData;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by Ynott on 7/28/15.
+ */
+public interface GhostCallAPIInterface {
+
+    @GET("/credit_packages")
+    List<CreditPackagesData> getCreditPackagesData();
+
+    @GET("/effects")
+    List<SoundEffectsData> getsoundEffectsList();
+
+    @FormUrlEncoded
+    @POST("/effects")
+    void sendEffects(@Field("effect_item_id") String effectID, @Field("resource_id") String resourceID, Callback<Response> soundEffectStatus);
+
+    @GET("/packages?type=new")
+    List<NumberPackagesData> getNewNumberPackages();
+
+    @GET("/packages?type=extend")
+    List<NumberPackagesData> getExtendNumberPackages();
+
+    @GET("/backgrounds")
+    List<BackgroundEffectsData> getBackgroundEffectsList();
+
+    @GET("/user")
+    UserData getUserData();
+
+    @GET("/call_status")
+    void getCallStatus(@Query("resource_id") String resourceID, Callback<CallStatus> statusCallback);
+
+    @GET("/user")
+    void getUserData(Callback<UserData> callBack);
+
+    @FormUrlEncoded
+    @POST("/hangup")
+    void hangUpCall(@Field("resource_id") String resourceID, Callback<Response> hangUpResponse);
+
+    @FormUrlEncoded
+    @POST("/calls")
+    void makeCall(@Field("to") String to, @Field("number_id") String numberID, @Field("background_item_id") String backgroundID, @Field("voicechanger") String voicechanger,
+            @Field("record") String record, @Field("use_verified_number") String verified, @Field("method") String method, Callback<CallData> callback);
+
+    @GET("/playback/call/{callID}/mp3")
+    void getMP3(@Path("callID") String callID, @Query("api_key") String api_key, Callback<Response> responseCallback);
+
+    @FormUrlEncoded
+    @POST("/messages")
+    void sendText(@Field("to") String to, @Field("number_id") String numberID, @Field("text") String text, Callback<Response> response);
+
+    @GET("/messages/{numberID}/{timestamp}")
+    List<Message> getMessages(@Path("numberID") String numberID, @Path("timestamp") String timeStamp);
+
+    @GET("/available_area_code/{area_code}")
+    AreaCodeObject getAreaCodeStatus(@Path("area_code") String areaCode);
+
+    @FormUrlEncoded
+    @POST("/purchase")
+    void purchaseCredits(@Field("type") String type, @Field("item") String item, @Field("token") String token, @Field("transaction_id") String transactionID, Callback<Response> response);
+
+    @FormUrlEncoded
+    @POST("/purchase")
+    void purchaseNewNumber(@Field("type") String type, @Field("item") String item, @Field("name") String nickName, @Field("area_code") String areaCode, @Field("token") String token, @Field("transaction_id") String transactionID, Callback<Response> response);
+
+    @FormUrlEncoded
+    @POST("/purchase")
+    void extendNumber(@Field("type") String type, @Field("item") String item, @Field("number_id") String numberID, @Field("token") String token, @Field("transaction_id") String transactionID, Callback<ExtendObject> extendObject);
+}
---
+++ GhostCall/app/src/main/java/com/kickbackapps/ghostcall/PackageAdapter.java
@@ -0,0 +1,81 @@
+package com.kickbackapps.ghostcall;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.List;
+
+
+public class PackageAdapter extends BaseAdapter {
+
+    private LayoutInflater layoutInflater;
+    private List<GhostPackage> ghostPackage;
+
+
+    public PackageAdapter(Context context, List<GhostPackage> ghostPackage) {
+
+        layoutInflater = LayoutInflater.from(context);
+        this.ghostPackage = ghostPackage;
+    }
+
+    @Override
+    public int getCount() {
+
+        return ghostPackage.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return ghostPackage.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view;
+        ViewHolder holder;
+        if(convertView == null) {
+            view = layoutInflater.inflate(R.layout.package_row, parent, false);
+            holder = new ViewHolder();
+            holder.packageName = (TextView) view.findViewById(R.id.packageName);
+            holder.packageTime = (TextView) view.findViewById(R.id.packageTime);
+            holder.packagePrice = (TextView) view.findViewById(R.id.packagePrice);
+            holder.packageCredit = (TextView) view.findViewById(R.id.packageCredits);
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (ViewHolder) view.getTag();
+        }
+
+        GhostPackage gPackage = ghostPackage.get(position);
+        holder.packageName.setText(gPackage.getPackageName());
+        if (gPackage.getPackageType().equals("new")) {
+            holder.packageTime.setText("Expires in " + gPackage.getPackageTime() + " days");
+            holder.packageCredit.setText(" - " + gPackage.getPackageCredits() + " Minutes Free");
+        } else if (gPackage.getPackageType().equals("extend")) {
+            holder.packageTime.setText("Extends for " + gPackage.getPackageTime() + " days");
+            holder.packageCredit.setText(" - " + gPackage.getPackageCredits() + " Minutes Free");
+        } else if (gPackage.getPackageType().equals("credits")) {
+            holder.packageTime.setText(gPackage.getPackageTime());
+            holder.packageCredit.setText("");
+        }
+        holder.packagePrice.setText("$" + gPackage.getPackagePrice());
+
+
+        return view;
+    }
+
+    private class ViewHolder {
+        public TextView packageName, packagePrice, packageTime, packageCredit;
+    }
+}
